4be13b449df82a28448fb21d9658ad0e93b19214
Rob Chamberlain <robert.chamberlain@vaimo.com>
2026-07-16 13:27:58 +0200

    Fixed user name

--- app/(main)/profile/[id]/page.tsx
+++ app/(main)/profile/[id]/page.tsx
@@ -13,23 +13,21 @@ import Link from "next/link"
 const ProfilePage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params
   const { data, error } = await getUserPosts(id)
-  
   const username = data![0].author
   const supabase = await createClient()
   const { data: { user } } = await supabase.auth.getUser();
 
   return (
     <div>
-      <h1>{user ? "All your threads" : `${username.username}'s threads`}</h1>
-
-        <div className="w-full flex flex-wrap gap-8">
-          {data && data.map(({ id, title, author, slug, image }) =>
-            <Link className="basis-[calc(100%/2-32px/2)] md:basis-[calc(100%/3-32px*2/3)] flex flex-col border-1 rounded-xl" key={id} href={`/${slug}`}>
-              <div className="w-full aspect-square flex align-center justify-center overflow-hidden">
-                {image && <img className=" w-full h-full object-contain" src={image} alt={title} />}</div>
-              <h2 className="text-2xl my-2 p-2">{title}</h2>
-            </Link>)}
-        </div>
+      <h1 className="text-4xl text-center mb-8">{user && user.id === id ? "All your threads" : `${username.username}'s threads`}</h1>
+      <div className="w-full flex flex-wrap gap-8">
+        {data && data.map(({ id, title, author, slug, image }) =>
+          <Link className="basis-[calc(100%/2-32px/2)] md:basis-[calc(100%/3-32px*2/3)] flex flex-col border-1 rounded-xl" key={id} href={`/${slug}`}>
+            <div className="w-full aspect-square flex align-center justify-center overflow-hidden">
+              {image && <img className=" w-full h-full object-contain" src={image} alt={title} />}</div>
+            <h2 className="text-2xl my-2 p-2">{title}</h2>
+          </Link>)}
+      </div>
     </div>
   )
 }
